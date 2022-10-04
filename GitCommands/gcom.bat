0841779f19ffe3ee90a204187208b82e3cdc3721
Matheus Wintruff <mathwintruffac@gmail.com>
2022-10-04 20:58:31 +0100

    Better echo

--- GitCommands/gcom.bat
+++ GitCommands/gcom.bat
@@ -1,8 +1,10 @@
 @echo off
-
-echo Making a commit with the message: %1
-
+echo ############  Getting Git Status Before Commit  ########
 git status
+echo ############  Git Commit Stage  ########################
+echo Making a commit with the message: %1
 git add .
 git commit -m %1
+echo ############  Getting Git Status After Commit  #########
 git status
+echo ############  Finished gcom  ###########################
